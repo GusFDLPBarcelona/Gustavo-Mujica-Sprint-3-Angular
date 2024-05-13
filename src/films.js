929b8e419692d57ff97b1e90fd90fc3df4f3adc4
Gus <151650535+GusFDLPBarcelona@users.noreply.github.com>
2024-05-13 10:22:01 +0200

    Ejerciciio 1 y 2del Nivel 1 con las funciones completas pasando los test.

--- src/films.js
+++ src/films.js
@@ -1,34 +1,111 @@
-// Exercise 1: Get the array of all directors.
+// Ejercicio 1: Get the array of all directors.
+
 function getAllDirectors(array) {
-  let result =  ???;
+
+  let result = array.map(movie => movie.director);
+
   console.log("EXERCICE 1 ->", result);
+
   return result;
+
 }
 
-// Exercise 2: Get the films of a certain director
-function getMoviesFromDirector(array, director) {
+// Ejercicio 2: Filtrar las películas por el nombre del director utilizando filter.
+function getMoviesFromDirector(movies, directorName) {
+  
+  const moviesFromDirector = movies.filter(movie => movie.director === directorName);
+  
+  console.log("Movies from director", directorName, ":", moviesFromDirector);
+  
+  return moviesFromDirector;
+}
+
+// Ejercicio 3: Para este ejercicio vamos a calcular el promedio de las puntuaciones de las películas de un director específico. Luego de sumar el total de las puntuaciones, se divide por la cantidad de películas. Al resultado lo redondea,os a dos decimales.
+
+function moviesAverageOfDirector(movies, director) {
+  
+  const directorMovies = movies.filter(movie => movie.director === director);
  
+  const totalScores = directorMovies.reduce((acc, movie) => acc + movie.score, 0);
+
+  const averageScore = totalScores / directorMovies.length;
+
+  const roundedAverage = parseFloat(averageScore.toFixed(2));
+
+  console.log(`El promedio de las películas dirigidas por ${director} es: ${roundedAverage}`);
+
+  return roundedAverage;
 }
 
-// Exercise 3: Calculate the average of the films of a given director.
-function moviesAverageOfDirector(array, director) {
+
+// Ejercicio 4:  Con este ejercicio vamos a ordenar las películas alfabéticamente. Para ello, primero ordenamos las películas por título y luego extraemos solo los títulos de las películas ordenadas. Finalmente, tomamos solo los primeros 20 títulos ordenados alfabéticamente. 
+
+function orderAlphabetically(movies) {
+ 
+  const sortedMovies = movies
+    .sort((a, b) => a.title.localeCompare(b.title))
+    .map(movie => movie.title);   
+
+  const top20Movies = sortedMovies.slice(0, 20);
+
+  console.log("Top 20 películas ordenadas alfabéticamente:", top20Movies );  
   
+  return top20Movies;
+}
+module.exports = { orderAlphabetically };
+
+
+// Ejercicio 5: Al ordenar las películas por año, si dos películas tienen el mismo año, se ordenarán por título. Y si son diferentes se ordenan por año.
+
+function orderByYear(movies) {
+
+  const sortedMovies = [...movies].sort((a, b) => {
+    if (a.year === b.year) {
+      
+      return a.title.localeCompare(b.title);
+    } else {
+     
+      return a.year - b.year;
+    }
+  });
+
+  console.log("Películas ordenadas por año:", sortedMovies);
+
+  return sortedMovies;
 }
 
-// Exercise 4:  Alphabetic order by title 
-function orderAlphabetically(array) {
+module.exports = { orderByYear };
+
+
+// Ejercicio 6: Transformar la duración de las películas de horas a minutos. Separamos la duración en horas y minutos, convertimos las horas a minutos y sumamos los minutos adicionales. Finalmente, devolvemos la película con la duración convertida a minutos.
+
+function hoursToMinutes(movies) {
   
+  const convertedMovies = movies.map(movie => {
+    const durationArray = movie.duration.split(' '); 
+    let totalMinutes = 0;
+
+    durationArray.forEach(time => {
+      if (time.includes('h')) {
+       
+        totalMinutes += parseInt(time) * 60;
+
+      } else if (time.includes('min')) {
+        
+        totalMinutes += parseInt(time);
+      }
+    });
+
+    return { ...movie, duration: totalMinutes }; // Devolver la película con la duración convertida a minutos
+  });
+
+  console.log("Movies with duration converted to minutes:", convertedMovies);
+
+  return convertedMovies;
 }
 
-// Exercise 5: Order by year, ascending
-function orderByYear() {
+module.exports = { hoursToMinutes };
 
-}
-
-// Exercise 6: Calculate the average of the movies in a category
-function moviesAverageByCategory() {
-
-}
 
 // Exercise 7: Modify the duration of movies to minutes
 function hoursToMinutes() {
